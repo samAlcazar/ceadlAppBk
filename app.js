7cb777513f5f87a7b53d2679b1e5dc279b92a027
saalcazar <alcazar.samuel@gmail.com>
2025-10-02 00:29:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import cookieParser from 'cookie-parser'
+import express from 'express'
+
+const PORT = process.env.PORT ?? 4984
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res) => {
+  res.status(404).send('404 Not Found')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
