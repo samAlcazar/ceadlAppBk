1265912d08ff9ee92fd0510589bb755fa1612ff8
saalcazar <alcazar.samuel@gmail.com>
2025-10-09 06:43:35 -0400

    feat: update user creation data and refine CORS accepted origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,11 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'http://192.168.40.110:5173',
-  'http://192.168.40.110:5174',
-  'http://192.168.40.110:1234'
+  'https://app.ceadl.org.bo',
+  'https://www.app.ceadl.org.bo',
+  'https://ceadl.org.bo',
+  'https://www.ceadl.org.bo',
+  'https://ceadlappbk-production.up.railway.app'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
